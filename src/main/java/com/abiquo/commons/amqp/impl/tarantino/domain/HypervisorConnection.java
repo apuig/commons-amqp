1500b5090f10e07cf533165c845f46508644402f
danielestevez <daniel.estevez@abiquo.com>
2011-10-03 17:58:19 +0200

    HyperV getConnectionURI implementation added

--- src/main/java/com/abiquo/commons/amqp/impl/tarantino/domain/HypervisorConnection.java
+++ src/main/java/com/abiquo/commons/amqp/impl/tarantino/domain/HypervisorConnection.java
@@ -27,123 +27,112 @@ import org.codehaus.jackson.annotate.JsonTypeInfo.As;
 import org.codehaus.jackson.annotate.JsonTypeInfo.Id;
 
 @JsonTypeInfo(use = Id.CLASS, include = As.PROPERTY, property = "@class")
-public class HypervisorConnection
-{
-    protected HypervisorType hypervisorType;
+public class HypervisorConnection {
+	protected HypervisorType hypervisorType;
 
-    protected String ip;
+	protected String ip;
 
-    protected String loginUser;
+	protected String loginUser;
 
-    protected String loginPassword;
+	protected String loginPassword;
 
-    public String getIp()
-    {
-        return ip;
-    }
+	public String getIp() {
+		return ip;
+	}
 
-    public void setIp(final String value)
-    {
-        this.ip = value;
-    }
+	public void setIp(final String value) {
+		this.ip = value;
+	}
 
-    public String getLoginUser()
-    {
-        return loginUser;
-    }
+	public String getLoginUser() {
+		return loginUser;
+	}
 
-    public void setLoginUser(final String value)
-    {
-        this.loginUser = value;
-    }
+	public void setLoginUser(final String value) {
+		this.loginUser = value;
+	}
 
-    public String getLoginPassword()
-    {
-        return loginPassword;
-    }
+	public String getLoginPassword() {
+		return loginPassword;
+	}
 
-    public void setLoginPassword(final String value)
-    {
-        this.loginPassword = value;
-    }
+	public void setLoginPassword(final String value) {
+		this.loginPassword = value;
+	}
 
-    public HypervisorType getHypervisorType()
-    {
-        return hypervisorType;
-    }
+	public HypervisorType getHypervisorType() {
+		return hypervisorType;
+	}
 
-    public void setHypervisorType(final HypervisorType hypervisorType)
-    {
-        this.hypervisorType = hypervisorType;
-    }
+	public void setHypervisorType(final HypervisorType hypervisorType) {
+		this.hypervisorType = hypervisorType;
+	}
 
-    @JsonIgnore
-    public String getConnectionURI()
-    {
-        return this.hypervisorType.getConnectionURI(getIp());
-    }
+	@JsonIgnore
+	public String getConnectionURI() {
+		return this.hypervisorType.getConnectionURI(getIp());
+	}
 
-    // TODO duplicated
-    public enum HypervisorType
-    {
-        /** Virtual Box */
-        VBOX(18083),
+	// TODO duplicated
+	public enum HypervisorType {
+		/** Virtual Box */
+		VBOX(18083),
 
-        /** KVM */
-        KVM(8889),
+		/** KVM */
+		KVM(8889),
 
-        /** XEN */
-        XEN_3(8889),
+		/** XEN */
+		XEN_3(8889),
 
-        /** ESXi */
-        VMX_04(443),
+		/** ESXi */
+		VMX_04(443),
 
-        /** Hyper V */
-        HYPERV_301(5985),
+		/** Hyper V */
+		HYPERV_301(5985),
 
-        /** Xen Server */
-        XENSERVER(9363),
+		/** Xen Server */
+		XENSERVER(9363),
 
-        /** Test actor */
-        TEST(56);
+		/** Test actor */
+		TEST(56);
 
-        public final int defaultPort;
+		public final int defaultPort;
 
-        private final int defaultAimPort = 8889;
+		private final int defaultAimPort = 8889;
 
-        public int getAimPort()
-        {
-            // TODO only if(this == VBOX || this == KVM || this == XEN_3)
-            return defaultAimPort;
-        }
+		public int getAimPort() {
+			// TODO only if(this == VBOX || this == KVM || this == XEN_3)
+			return defaultAimPort;
+		}
 
-        private HypervisorType(final int port)
-        {
-            this.defaultPort = port;
-        }
+		private HypervisorType(final int port) {
+			this.defaultPort = port;
+		}
 
-        public String getConnectionURI(final String ip)
-        {
-            switch (this)
-            {
-                case VMX_04:
-                    return String.format("https://%s:%d/sdk", ip, defaultPort);
+		public String getConnectionURI(final String ip) {
+			switch (this) {
+			case VMX_04:
+				return String.format("https://%s:%d/sdk", ip, defaultPort);
 
-                case KVM:
-                    return String.format("qemu+tcp://%s/system?no_tty=1", ip);
+			case KVM:
+				return String.format("qemu+tcp://%s/system?no_tty=1", ip);
 
-                case XEN_3:
-                    return String.format("xen+tcp://%s/?no_tty=1", ip);
+			case XEN_3:
+				return String.format("xen+tcp://%s/?no_tty=1", ip);
 
-                case VBOX:
-                    return String.format("http://%s:%d", ip, defaultPort);
+			case VBOX:
+				return String.format("http://%s:%d", ip, defaultPort);
 
-                case XENSERVER:
-                    return String.format("http://%s", ip);
+			case XENSERVER:
+				return String.format("http://%s", ip);
 
-                default:
-                    throw new RuntimeException("Unable to build the connection URI, unknown hypervisor.");
-            }
-        }
-    }
+			case HYPERV_301:
+				return String.format("http://%s:%d", ip, defaultPort);
+
+			default:
+				throw new RuntimeException(
+						"Unable to build the connection URI, unknown hypervisor.");
+			}
+		}
+	}
 }
